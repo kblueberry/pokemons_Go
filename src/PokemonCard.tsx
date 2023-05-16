2b37c9e24c53eec9f7d7ed75ce60c28489e3d9c0
Khrystyna Derenivska <khrisderenivska@gmail.com>
2023-05-16 16:20:07 +0300

    dto for pokemon details + api call to fetch details

--- src/PokemonCard.tsx
+++ src/PokemonCard.tsx
@@ -1,16 +1,29 @@
 import './PokemonCard.css';
+import { useEffect, useState } from "react";
 
 type PokemonProps = {
   info: PokemonInfo
 }
 
 function PokemonCard({info}: PokemonProps) {
+  const [pokemonInfo, setPokemonInfo] = useState<PokemonDetails>(null);
+
+  const getPokemonInfo = async () => {
+    const response = await fetch(`${info.url}`);
+    const responseToJson = await response.json();
+    console.log('Response=', responseToJson);
+
+    setPokemonInfo(responseToJson);
+    console.log('Details=', pokemonInfo);
+  };
+
   return (
       <>
         <div className="pokemon_info">
           <h5>{info.name.charAt(0).toUpperCase()
               + info.name.slice(1)}</h5>
-          <a className="pokemon_details_open_action">
+          <a className="pokemon_details_open_action"
+             onClick={getPokemonInfo}>
             Get info
           </a>
         </div>
--- src/dto.ts
+++ src/dto.ts
@@ -9,3 +9,74 @@ type PokemonsDataChunk = {
   previous: string,
   results: Array<PokemonInfo>
 }
+
+type PokemonDetails = {
+  abilities: Array<{
+    ability: PokemonInfo,
+    is_hidden: boolean,
+    slot: number
+  }>,
+  base_experience: number,
+  forms: Array<PokemonInfo>,
+  game_indices: Array<{
+    game_index: number,
+    version: PokemonInfo
+  }>,
+  height: number,
+  held_items: Array<any>,
+  id: number,
+  is_default: boolean,
+  location_area_encounters: string,
+  moves: Array<{
+    move: PokemonInfo,
+    version_group_details: Array<{
+      level_learned_at: number,
+      move_learn_method: PokemonInfo,
+      version_group: PokemonInfo
+    }>
+  }>,
+  name: string,
+  order: number,
+  past_types: Array<any>,
+  species: PokemonInfo,
+  sprites: Sprite,
+  stats: {
+    base_stat: number,
+    effort: number,
+    stat: PokemonInfo
+  },
+  types: Array<{
+    slot: number,
+    type: PokemonInfo
+  }>,
+  weight: number
+}
+
+type Sprite = {
+  back_default: string,
+  back_female: boolean,
+  back_shiny: string,
+  back_shiny_female: boolean,
+  front_default: string,
+  front_female: boolean,
+  front_shiny: string,
+  front_shiny_female: boolean,
+  other: {
+    dream_world: {
+      front_default: string,
+      front_female: null
+    },
+    home: {
+      front_default: string,
+      front_female: null,
+      front_shiny: string,
+      front_shiny_female: null
+    },
+    official-artwork: {
+      front_default: string,
+      front_shiny: string
+    }
+  },
+  versions: any,
+}
+
