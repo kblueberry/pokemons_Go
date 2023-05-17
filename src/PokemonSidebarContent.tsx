52f221e2fe0e7f585355018cb5eb357824cf572b
Khrystyna Derenivska <khrisderenivska@gmail.com>
2023-05-17 17:56:02 +0300

    load spinner || sidebar content dynamically

--- src/PokemonSidebarContent.tsx
+++ src/PokemonSidebarContent.tsx
@@ -1,24 +1,28 @@
-import './SideBarPokemonCard.css';
+import './PokemonSidebarContent.css';
+import './spinner.css';
 import { usePokemonDetails } from "./PokemonDetailsProvider";
 
 function PokemonSidebarContent() {
   const {loading, error, data} = usePokemonDetails();
+  const loadingSpinner = <div className="loader"></div>
 
   return <aside>
-    <h4>Hello</h4>
-    <table width="100%" border="0">
-      <tbody>
-      <tr valign="top">
-        <td width="80%">
-          <p>JJ</p>
-        </td>
+    {loading ? loadingSpinner : <>
+      <h4>Hello</h4>
+      <table width="100%" border="0">
+        <tbody>
+        <tr valign="top">
+          <td width="80%">
+            <p>JJ</p>
+          </td>
 
-        <td width="20%">
-          <p>II</p>
-        </td>
-      </tr>
-      </tbody>
-    </table>
+          <td width="20%">
+            <p>II</p>
+          </td>
+        </tr>
+        </tbody>
+      </table>
+    </>}
   </aside>
 }
 
